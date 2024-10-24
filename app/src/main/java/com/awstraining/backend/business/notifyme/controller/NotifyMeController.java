1f581d571b4bf2d44bcb01a8e41f760c696a6de2
szymciq <99139242+szymciq@users.noreply.github.com>
2024-10-24 14:22:05 +0200

    Lab1

--- app/src/main/java/com/awstraining/backend/business/notifyme/controller/NotifyMeController.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/controller/NotifyMeController.java
@@ -4,7 +4,11 @@ import com.awstraining.backend.api.rest.v1.NotifyMeApi;
 import com.awstraining.backend.api.rest.v1.model.NotifyMe;
 import com.awstraining.backend.api.rest.v1.model.SentMessage;
 import com.awstraining.backend.business.notifyme.NotifyMeDO;
+import com.awstraining.backend.business.notifyme.NotifyMeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 // TODO: lab1
@@ -16,17 +20,19 @@ public class NotifyMeController implements NotifyMeApi {
     
     // TODO: lab1
     //  1. Inject service with business logic.
-//    @Autowired
-    public NotifyMeController() {
+    @Autowired
+    public NotifyMeController(final NotifyMeService service) {
+        this.service = service;
     }
-
 
     // TODO: lab1
     //  1. Implement notifyMe method. 
     //  Method should return Http 200 and content of sent message to subscribers.
     @Override
     public ResponseEntity<SentMessage> notifyMe(NotifyMe notifyMe) {
-        return ResponseEntity.notFound().build();
+        final NotifyMeDO notifyMeDO = map(notifyMe);
+        final String message = service.notifyMe(notifyMeDO);
+        return ResponseEntity.ok(map(message));
     }
 
     private static SentMessage map(String message) {
